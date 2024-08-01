07ef92e66c08957e5668b1b4d73018839f0281c4
l02 <l8556@outlook.com>
2024-08-01 14:26:21 +0300

    Added methods for working with ssh keys

--- digitalocean_wrapper/digitalocean_wrapper.py
+++ digitalocean_wrapper/digitalocean_wrapper.py
@@ -6,6 +6,7 @@ import digitalocean
 from .auth import Token
 from .digitalocean_prodjects import DigitalOceanProjects
 from .droplet import Droplet
+from .ssh_key import SSHKey
 
 
 class DigitalOceanWrapper:
@@ -15,22 +16,13 @@ class DigitalOceanWrapper:
         self.manager = digitalocean.Manager(token=self.__token)
         self.project = DigitalOceanProjects(manager=self.manager)
         self.droplet = Droplet(do_token=self.__token, manager=self.manager, project=self.project)
+        self.ssh_key = SSHKey(manager=self.manager, do_token=self.__token)
 
     def get_ssh_keys(self) -> list[digitalocean.SSHKey]:
-        return self.manager.get_all_sshkeys()
+        return self.ssh_key.get_all()
 
     def get_all_ssh_keys_id(self) -> list:
-        ssh_keys = self.get_ssh_keys()
-        if ssh_keys:
-            return [key.id for key in ssh_keys]
-        return []
+        return self.ssh_key.get_all_id()
 
     def get_ssh_key_id_by_name(self, ssh_key_name: str) -> Optional[int]:
-        ssh_keys = self.get_ssh_keys()
-
-        for ssh_key in ssh_keys if ssh_keys else []:
-            if ssh_key.name.lower() == ssh_key_name.lower():
-                return ssh_key.id
-
-        print(f"[red]|ERROR| Ssh key id for {ssh_key_name} was not found.")
-        return None
+        return self.ssh_key.get_id_by_name(ssh_key_name=ssh_key_name)
---
+++ digitalocean_wrapper/ssh_key/ssh_key.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+from os.path import join, expanduser, isfile
+from typing import Optional
+from rich import print
+
+import digitalocean
+
+class SSHKeyError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+        def __str__(self):
+            print(self.message)
+            return self.message
+
+class SSHKey:
+    default_pub_key_path = join(expanduser('~'), '.ssh', 'id_rsa.pub')
+
+    def __init__(self, manager: digitalocean.Manager, do_token: str):
+        self.__manager = manager
+        self.__token = do_token
+
+    def get_all(self) -> list[digitalocean.SSHKey]:
+        return self.__manager.get_all_sshkeys()
+
+    def create(self, key_name: str, public_key: str = None, stdout: bool = True) -> bool:
+        if self.check_key_name_exists(key_name=key_name):
+            raise SSHKeyError(
+                f"[red]|ERROR| The ssh public key named {key_name} already exists on DigitalOcean. Select another name"
+            )
+
+        ssh_pub_key = public_key or self._read_default_pub_key()
+        ssh_key = self.get_by_pub_key(public_key=ssh_pub_key, stderr=True)
+
+        if ssh_key is not None:
+            raise SSHKeyError(
+                f"[red]|ERROR| The ssh public key already exists on DigitalOcean under the name: {ssh_key.name}"
+            )
+
+        digitalocean.SSHKey(token=self.__token, name=key_name, public_key=ssh_pub_key).create()
+
+        if self.get_id_by_name(ssh_key_name=key_name):
+            print(f"[green]|INFO| The ssh key `{key_name}` created on DigitalOcean") if stdout else None
+            return True
+
+        print(f"[red]|ERROR| The ssh key `{key_name}` was not found.") if stdout else None
+        return False
+
+
+    def get_all_ssh_key_names(self) -> list[str]:
+        return [ssh_key.name for ssh_key in self.get_all()]
+
+    def get_by_pub_key(self, public_key: str = None, stderr: bool = False) -> Optional[digitalocean.SSHKey]:
+        pub_key = public_key or self._read_default_pub_key()
+
+        if pub_key:
+            return digitalocean.SSHKey(token=self.__token).load_by_pub_key(pub_key)
+
+        print(f"[red]|WARNING| Cannot find the ssh key {public_key or self.default_pub_key_path}") if stderr else None
+        return None
+
+    def get_by_id(self, key_id: int) -> Optional[digitalocean.SSHKey]:
+        try:
+            return self.__manager.get_ssh_key(ssh_key_id=key_id)
+        except digitalocean.DataReadError:
+            print(f"[red]|ERROR| SSH key with ID {key_id} was not found.")
+            return None
+
+    def get_by_name(self, key_name: str, stderr: bool = False) -> Optional[digitalocean.SSHKey]:
+        for ssh_key in self.get_all():
+            if ssh_key.name == key_name:
+                return ssh_key
+        print(f"[red]|WARNING| Cannot find the ssh key") if stderr else None
+        return None
+
+    def get_all_id(self):
+        ssh_keys = self.get_all()
+        if ssh_keys:
+            return [key.id for key in ssh_keys]
+        return []
+
+    def get_id_by_name(self, ssh_key_name: str, stderr: bool = False) ->  Optional[int]:
+        ssh_keys = self.get_all()
+
+        for ssh_key in ssh_keys if ssh_keys else []:
+            if ssh_key.name.lower() == ssh_key_name.lower():
+                return ssh_key.id
+
+        print(f"[red]|ERROR| Ssh key id for {ssh_key_name} was not found.") if stderr else None
+        return None
+
+    def check_key_name_exists(self, key_name: str) -> bool:
+        return key_name in self.get_all_ssh_key_names()
+
+    def _read_default_pub_key(self) -> Optional[str]:
+        if isfile(self.default_pub_key_path):
+            with open(self.default_pub_key_path, mode='r') as file:
+                return file.read().strip()
+        return None
+
+    def _get_ssh_key(self, ssh_key:  digitalocean.SSHKey | str | int) -> digitalocean.SSHKey:
+        if isinstance(ssh_key, digitalocean.SSHKey):
+            return ssh_key
+        elif isinstance(ssh_key, str):
+            return self.get_by_name(key_name=ssh_key)
+        elif isinstance(ssh_key, int):
+            return self.get_by_id(ssh_key)
