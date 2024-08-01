af12caa89ff2463a56a2a668e969a5580a4902d8
l02 <l8556@outlook.com>
2024-08-01 14:27:33 +0300

    Delete old methods

--- digitalocean_wrapper/digitalocean_wrapper.py
+++ digitalocean_wrapper/digitalocean_wrapper.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from typing import Optional
-
 import digitalocean
 
 from .auth import Token
@@ -17,12 +15,3 @@ class DigitalOceanWrapper:
         self.project = DigitalOceanProjects(manager=self.manager)
         self.droplet = Droplet(do_token=self.__token, manager=self.manager, project=self.project)
         self.ssh_key = SSHKey(manager=self.manager, do_token=self.__token)
-
-    def get_ssh_keys(self) -> list[digitalocean.SSHKey]:
-        return self.ssh_key.get_all()
-
-    def get_all_ssh_keys_id(self) -> list:
-        return self.ssh_key.get_all_id()
-
-    def get_ssh_key_id_by_name(self, ssh_key_name: str) -> Optional[int]:
-        return self.ssh_key.get_id_by_name(ssh_key_name=ssh_key_name)
