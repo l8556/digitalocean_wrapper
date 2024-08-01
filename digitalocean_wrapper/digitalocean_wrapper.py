0667e921f63aff605697e736f0a7f4402913d9fc
Nikolai Lambrov <52069739+l8556@users.noreply.github.com>
2024-08-01 15:55:41 +0300

    Feature/add ssh key methods (#2)
    * Added methods for working with ssh keys
    
    * Delete old methods
    
    * added methods for getting ssh key information
    
    * added methods for getting ssh key information

--- digitalocean_wrapper/digitalocean_wrapper.py
+++ digitalocean_wrapper/digitalocean_wrapper.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-from typing import Optional
-
 import digitalocean
 
 from .auth import Token
 from .digitalocean_prodjects import DigitalOceanProjects
 from .droplet import Droplet
+from .ssh_key import SSHKey
 
 
 class DigitalOceanWrapper:
@@ -15,22 +14,4 @@ class DigitalOceanWrapper:
         self.manager = digitalocean.Manager(token=self.__token)
         self.project = DigitalOceanProjects(manager=self.manager)
         self.droplet = Droplet(do_token=self.__token, manager=self.manager, project=self.project)
-
-    def get_ssh_keys(self) -> list[digitalocean.SSHKey]:
-        return self.manager.get_all_sshkeys()
-
-    def get_all_ssh_keys_id(self) -> list:
-        ssh_keys = self.get_ssh_keys()
-        if ssh_keys:
-            return [key.id for key in ssh_keys]
-        return []
-
-    def get_ssh_key_id_by_name(self, ssh_key_name: str) -> Optional[int]:
-        ssh_keys = self.get_ssh_keys()
-
-        for ssh_key in ssh_keys if ssh_keys else []:
-            if ssh_key.name.lower() == ssh_key_name.lower():
-                return ssh_key.id
-
-        print(f"[red]|ERROR| Ssh key id for {ssh_key_name} was not found.")
-        return None
+        self.ssh_key = SSHKey(manager=self.manager, do_token=self.__token)
---
+++ digitalocean_wrapper/ssh_key/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from .ssh_key import SSHKey
